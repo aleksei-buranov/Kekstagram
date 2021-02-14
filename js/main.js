a30f363775065d4c71623830ddfb6faf6d534273
smokehawk <33549625+smokehawk@users.noreply.github.com>
2021-02-15 00:21:25 +0500

    Finished creating photo description function, added picture rendering based on the description

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 var OBJECTS_COUNT = 25;
 var AVATARS_COUNT = 6;
+var COMMENTS_MIN = 3;
+var COMMENTS_MAX = 10;
 var USER_NAMES = [
   "Максим",
   "Александр",
@@ -23,9 +25,46 @@ var USER_MESSAGES = [
   "Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ?!",
 ];
 
+var PICTURE_TEMPLATE = document
+  .querySelector("#picture")
+  .content.querySelector(".picture");
+var PICTURES = document.querySelector(".pictures");
+
 var getRandomInteger = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
+};
+
+var shuffleArray = function (a) {
+  var j;
+  var x;
+  var i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+};
+
+var generateIntegerArray = function (count) {
+  var a = [];
+  for (var i = 0; i < count; i++) {
+    a[i] = i + 1;
+  }
+  return a;
+};
+
+var generateDescriptionsArray = function (count) {
+  var photoIndexes = generateIntegerArray(count);
+  photoIndexes = shuffleArray(photoIndexes);
+  var descriptionsArray = [];
+  for (var i = 0; i < count; i++) {
+    var photoIndex = photoIndexes.splice(-1, 1);
+    descriptionsArray[i] = generateUserDescription(photoIndex);
+  }
+  return descriptionsArray;
 };
 
 var generateComment = function () {
@@ -45,14 +84,35 @@ var generateCommentsArray = function (count) {
   return commentsArray;
 };
 
-var generateUserDescription = function () {
+var generateUserDescription = function (photoIndex) {
   var userDescription = {
-    url: "photos/.jpg",
+    url: "photos/" + photoIndex + ".jpg",
     description: "Фотография",
     likes: getRandomInteger(15, 200),
-    comments: generateCommentsArray(getRandomInteger(3, 10)),
+    comments: generateCommentsArray(
+      getRandomInteger(COMMENTS_MIN, COMMENTS_MAX)
+    ),
   };
   return userDescription;
 };
 
-console.log(generateUserDescription());
+var renderPictures = function (picDescriptions) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < picDescriptions.length; i++) {
+    var pictureElement = PICTURE_TEMPLATE.cloneNode(true);
+    var pictureImg = pictureElement.querySelector(".picture__img");
+    var pictureLikes = pictureElement.querySelector(".picture__likes");
+    var pictureComments = pictureElement.querySelector(".picture__comments");
+
+    pictureImg.src = picDescriptions[i].url;
+    pictureLikes.textContent = picDescriptions[i].likes;
+    pictureComments.textContent = picDescriptions[i].comments.length;
+
+    fragment.append(pictureElement);
+  }
+  PICTURES.append(fragment);
+};
+
+var picDescriptions = generateDescriptionsArray(OBJECTS_COUNT);
+
+renderPictures(picDescriptions);
